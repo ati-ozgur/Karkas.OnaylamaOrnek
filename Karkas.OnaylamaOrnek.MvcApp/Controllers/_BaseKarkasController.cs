cc0fa04107a563d783a770837dd41fa6a8567ae4
Atilla Ozgur <ati.ozgur@gmail.com>
2013-08-02 21:37:05 +0300

    BaseController eklendi.

---
+++ Karkas.OnaylamaOrnek.MvcApp/Controllers/_BaseKarkasController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Karkas.OnaylamaOrnek.MvcApp.Controllers
+{
+    public class _BaseKarkasController : Controller
+    {
+        public void MesajEkleUyari(string mesaj)
+        {
+            TempData.Add(Alarmlar.UYARI, mesaj);
+        }
+        public void MesajEkleBasari(string mesaj)
+        {
+            TempData.Add(Alarmlar.BASARILI, mesaj);
+        }
+
+        public void MesajEkleBilgi(string mesaj)
+        {
+            TempData.Add(Alarmlar.BILGI, mesaj);
+        }
+
+        public void MesajEkleHata(string mesaj)
+        {
+            TempData.Add(Alarmlar.HATA, mesaj);
+        }
+
+    }
+
+
+    public static class Alarmlar
+    {
+        public const string BASARILI = "success";
+        public const string UYARI = "attention";
+        public const string HATA = "error";
+        public const string BILGI = "info";
+
+        public static string[] ALL
+        {
+            get { return new[] { BASARILI, UYARI, HATA, BILGI }; }
+        }
+    }
+
+}
