ff392106bc4dbda2bb54fb8cebdc42176be42e32
Atilla Ozgur <ati.ozgur@gmail.com>
2013-08-02 21:43:34 +0300

    log desteği eklendi.

--- Karkas.OnaylamaOrnek.MvcApp/Controllers/_BaseKarkasController.cs
+++ Karkas.OnaylamaOrnek.MvcApp/Controllers/_BaseKarkasController.cs
@@ -3,28 +3,36 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using log4net;
+
 
 namespace Karkas.OnaylamaOrnek.MvcApp.Controllers
 {
     public class _BaseKarkasController : Controller
     {
+        private static readonly ILog log = LogManager.GetLogger(typeof(_BaseKarkasController));
+
         public void MesajEkleUyari(string mesaj)
         {
             TempData.Add(Alarmlar.UYARI, mesaj);
+            log.Warn(mesaj);
         }
         public void MesajEkleBasari(string mesaj)
         {
             TempData.Add(Alarmlar.BASARILI, mesaj);
+            log.Debug(mesaj);
         }
 
         public void MesajEkleBilgi(string mesaj)
         {
             TempData.Add(Alarmlar.BILGI, mesaj);
+            log.Info(mesaj);
         }
 
         public void MesajEkleHata(string mesaj)
         {
             TempData.Add(Alarmlar.HATA, mesaj);
+            log.Error(mesaj);
         }
 
     }
