a5ea3bf70b57b625ce85fc7f1a4cd78a89d3a9bb
Atilla Ozgur <ati.ozgur@gmail.com>
2013-08-02 23:20:24 +0300

    ufak düzeltme

--- Karkas.OnaylamaOrnek.MvcApp/App_Start/RouteConfig.cs
+++ Karkas.OnaylamaOrnek.MvcApp/App_Start/RouteConfig.cs
@@ -15,8 +15,8 @@ namespace Karkas.OnaylamaOrnek.MvcApp
 
             routes.MapRoute(
                 name: "Default",
-                url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                url: "{controller}/{action}/{key}",
+                defaults: new { controller = "Home", action = "Index", key = UrlParameter.Optional }
             );
         }
     }
